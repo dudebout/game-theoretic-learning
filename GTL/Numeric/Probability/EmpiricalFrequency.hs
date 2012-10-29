056a048e12d5199069faba5caf4e9585475b7d1b
Nicolas Dudebout <nicolas.dudebout@gatech.edu>
2012-10-29 15:40:32 -0400

    Simplifies the empirical frequency calculation.
    Only a State monad was needed and not a full RWS stack.

--- GTL/Numeric/Probability/EmpiricalFrequency.hs
+++ GTL/Numeric/Probability/EmpiricalFrequency.hs
@@ -2,8 +2,8 @@ module GTL.Numeric.Probability.EmpiricalFrequency ( addObservation
                                                   , empiricalFrequencyStepSize
                                                   , computeEmpiricalFrequencies ) where
 
-import Control.Monad.RWS
-import Control.Monad.Loops (whileM_)
+import Control.Monad.State
+import Control.Monad.Loops (whileM)
 import GTL.Numeric.Probability (Dist)
 import Numeric.Probability.Distribution (relative, certainly, unfold)
 import GTL.Data.Unitless (StepSize)
@@ -19,30 +19,25 @@ data Accumulator a = Accumulator { observations :: [a]
                                  , time         :: Time
                                  , distribution :: Dist a }
 
-accumulate :: (Ord a, Monoid w) => RWS () w (Accumulator a) ()
-accumulate = do
+oneStep :: Ord a => State (Accumulator a) (Dist a)
+oneStep = do
   Accumulator { observations = observs
               , time         = t
               , distribution = dist } <- get
   let obs = head observs
       d'  = addObservation dist obs $ empiricalFrequencyStepSize t
   put $ Accumulator (tail observs) (t + 1) d'
+  return d'
 
-reportDist :: RWS () [Dist a] (Accumulator a) ()
-reportDist = gets distribution >>= tell . (:[])
-
-notDoneM :: (Monoid w) => RWS () w (Accumulator a) Bool
+notDoneM :: State (Accumulator a) Bool
 notDoneM = fmap notDone get
 
 notDone :: Accumulator a -> Bool
 notDone = not . null . observations
 
-oneStep :: Ord a => RWS () [Dist a] (Accumulator a) ()
-oneStep = accumulate >> reportDist
-
-complete :: Ord a => RWS () [Dist a] (Accumulator a) ()
-complete = whileM_ notDoneM oneStep
+complete :: Ord a => State (Accumulator a) [Dist a]
+complete = whileM notDoneM oneStep
 
 computeEmpiricalFrequencies :: Ord a => [a] -> [Dist a]
-computeEmpiricalFrequencies []         = []
-computeEmpiricalFrequencies as@(a:_) = snd $ evalRWS complete () $ Accumulator as 1 (certainly $ a)
+computeEmpiricalFrequencies []       = []
+computeEmpiricalFrequencies as@(a:_) = evalState complete $ Accumulator as 1 (certainly $ a)
