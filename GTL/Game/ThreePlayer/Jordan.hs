4330d42fd3162303a1b0c6782934c321ea011716
Nicolas Dudebout <nicolas.dudebout@gatech.edu>
2012-10-30 18:16:00 -0400

    Cleans up the Shapley and Jordan games.

--- GTL/Game/ThreePlayer/Jordan.hs
+++ GTL/Game/ThreePlayer/Jordan.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE TypeOperators #-}
+
 module GTL.Game.ThreePlayer.Jordan ( module GTL.Game.ThreePlayer
                                    , Action1(..), Signal1, u1
                                    , Action2(..), Signal2, u2
                                    , Action3(..), Signal3, u3
+                                   , Actions, Signals
                                    ) where
 
 import GTL.Game.ThreePlayer
+import GTL.Data.Utility (UtilityAS)
 import Data.Ix (Ix)
+import Data.HList (HNil, (:*:))
 
 data Action1 = H1 | T1 deriving (Show, Bounded, Ix, Eq, Ord)
 data Action2 = H2 | T2 deriving (Show, Bounded, Ix, Eq, Ord)
@@ -15,17 +20,20 @@ type Signal1 = (Action2, Action3)
 type Signal2 = (Action1, Action3)
 type Signal3 = (Action1, Action2)
 
-u1 :: Action1 -> Signal1 -> Double
+u1 :: UtilityAS Action1 Signal1
 u1 H1 (H2, _) = 1
 u1 T1 (T2, _) = 1
 u1 _  _       = -1
 
-u2 :: Action2 -> Signal2 -> Double
+u2 :: UtilityAS Action2 Signal2
 u2 H2 (_, H3) = 1
 u2 T2 (_, T3) = 1
 u2 _  _       = -1
 
-u3 :: Action3 -> Signal3 -> Double
+u3 :: UtilityAS Action3 Signal3
 u3 H3 (T1, _) = 1
 u3 T3 (H1, _) = 1
 u3 _  _       = -1
+
+type Actions = Action1 :*: Action2 :*: Action3 :*: HNil
+type Signals = Signal1 :*: Signal2 :*: Signal3 :*: HNil
--- GTL/Game/TwoPlayer/Shapley.hs
+++ GTL/Game/TwoPlayer/Shapley.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE TypeOperators #-}
+
 module GTL.Game.TwoPlayer.Shapley ( module GTL.Game.TwoPlayer
                                   , Action1(..), Signal1, u1
                                   , Action2(..), Signal2, u2
+                                  , Actions, Signals
                                   ) where
 
 import GTL.Game.TwoPlayer
+import GTL.Data.Utility (UtilityAS)
 import Data.Ix (Ix)
+import Data.HList (HNil, (:*:))
+
 
 data Action1 = U | M | D deriving (Show, Bounded, Ix, Eq, Ord)
 type Signal1 = Action2
@@ -12,14 +18,17 @@ type Signal1 = Action2
 data Action2 = L | C | R deriving (Show, Bounded, Ix, Eq, Ord)
 type Signal2 = Action1
 
-u1 :: Action1 -> Signal1 -> Double
+u1 :: UtilityAS Action1 Signal1
 u1 D L = 1
 u1 U C = 1
 u1 M R = 1
 u1 _ _ = 0
 
-u2 :: Action2 -> Signal2 -> Double
+u2 :: UtilityAS Action2 Signal2
 u2 R U = 1
 u2 L M = 1
 u2 C D = 1
 u2 _ _ = 0
+
+type Actions = Action1 :*: Action2 :*: HNil
+type Signals = Signal1 :*: Signal2 :*: HNil
